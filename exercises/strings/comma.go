3998d9a5092f8cbe7ed038e9fed6dc1559b2db19
Sergiy Sych <sychsergiy@gmail.com>
2019-08-19 20:51:56 +0300

    insert commas to Float number

--- exercises/strings/comma.go
+++ exercises/strings/comma.go
@@ -12,30 +12,65 @@ func commaRecursive(s string) string {
 	return commaRecursive(s[:n-3]) + "," + s[n-3:]
 }
 
-func commaLoop(s string) string {
+func splitOnChunksFromStart(s string, chunkSize int) []string {
+	// for IntegerPart
+	var chunks []string
+	sLen := len(s)
+	rest := sLen % chunkSize
+	if rest > 0 {
+		chunks = append(chunks, s[:rest])
+	}
+	chunks = append(chunks, splitOnChunks(s[rest:], chunkSize)...)
+	return chunks
+}
+
+func splitOnChunksFromEnd(s string, chunkSize int) []string {
+	// for Float part
+	sLen := len(s)
+	rest := sLen % chunkSize
+	chunks := splitOnChunks(s[:sLen-rest], chunkSize)
+	if rest > 0 {
+		chunks = append(chunks, s[sLen-rest:])
+	}
+	return chunks
+}
+
+func splitOnChunks(s string, chunkSize int) []string {
 	var chunks []string
 
 	sLen := len(s)
-	offset := sLen % 3
-
-	if offset > 0 {
-		chunks = append(chunks, s[:offset])
+	if sLen%chunkSize != 0 {
+		panic("Wrong input argument")
 	}
-	for i := offset; i < sLen; i += 3 {
-		chunks = append(chunks, s[i:i+3])
+	for i := 0; i < len(s); i += chunkSize {
+		chunks = append(chunks, s[i:i+chunkSize])
 	}
+	return chunks
+}
 
+func insertCommasToIntegerPart(s string, chunkSize int) string {
+	chunks := splitOnChunksFromStart(s, chunkSize)
 	return strings.Join(chunks, ",")
 }
 
-func commaFloatNumber(s string) string {
-	chunks := strings.Split(s, ",")
-	if len(chunks) > 2 {
+func insertCommasToFloatPart(s string, chunkSize int) string {
+	chunks := splitOnChunksFromEnd(s, chunkSize)
+	return strings.Join(chunks, ",")
+}
+
+func insertCommas(s string, chunkSize int) string {
+	chunks := strings.Split(s, ".")
+	if len(chunks) == 1 {
+		intPart := chunks[0]
+		return insertCommasToIntegerPart(intPart, chunkSize)
+	}
+	if len(chunks) == 2 {
+		intPart, floatPart := chunks[0], chunks[1]
+		intPartWithCommas := insertCommasToIntegerPart(intPart, chunkSize)
+		floatPartWithCommas := insertCommasToFloatPart(floatPart, chunkSize)
+		return intPartWithCommas + "." + floatPartWithCommas
+
+	} else {
 		panic("Wrong input")
 	}
-	intPart, floatPart := chunks[0], chunks[1]
-
-	intPartWithCommas := commaLoop(intPart)
-	floatPartWithCommas := commaLoop(floatPart)
-	return intPartWithCommas + "." + floatPartWithCommas
 }
--- exercises/strings/comma_test.go
+++ exercises/strings/comma_test.go
@@ -10,18 +10,35 @@ func Test_commaRecursive(t *testing.T) {
 	assert.Equal(t, "10,000,001", result)
 }
 
-func Test_commaLoop(t *testing.T) {
-	result := commaLoop("10000001")
+func Test_insertCommasToInteger(t *testing.T) {
+	result := insertCommasToIntegerPart("10000001", 3)
 	assert.Equal(t, "10,000,001", result)
 
-	result2 := commaLoop("100")
+	result2 := insertCommasToIntegerPart("100", 3)
 	assert.Equal(t, "100", result2)
 
-	result3 := commaLoop("10")
+	result3 := insertCommasToIntegerPart("10", 3)
 	assert.Equal(t, "10", result3)
 }
 
-func Test_commaFloatNumber(t *testing.T) {
-	result := commaLoop("1000.0001")
+func Test_insertCommasToFloat(t *testing.T) {
+	result := insertCommasToFloatPart("10000001", 3)
+	assert.Equal(t, "100,000,01", result)
+
+	result2 := insertCommasToFloatPart("100", 3)
+	assert.Equal(t, "100", result2)
+
+	result3 := insertCommasToFloatPart("10", 3)
+	assert.Equal(t, "10", result3)
+}
+
+func Test_insertCommas(t *testing.T) {
+	result := insertCommas("1000.0001", 3)
 	assert.Equal(t, "1,000.000,1", result)
+
+	result2 := insertCommas("100.01", 2)
+	assert.Equal(t, "1,00.01", result2)
+
+	result3 := insertCommas("1000", 2)
+	assert.Equal(t, "10,00", result3)
 }
