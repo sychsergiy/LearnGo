6bdcc930dd5b15b613f9d5ad3a5819a31b0c8e2a
Sergiy Sych <sychsergiy@gmail.com>
2019-08-20 10:55:12 +0300

    rotate slice by copy

--- exercises/data_structures/slices.go
+++ exercises/data_structures/slices.go
@@ -12,3 +12,21 @@ func reverseArray(sPtr *[]int) {
 		s[i], s[j] = s[j], s[i]
 	}
 }
+
+func rotate(s []int, n int) {
+	// Rotate s left by two positions.
+	reverse(s[:n])
+	reverse(s[n:])
+	reverse(s)
+}
+
+func rotateByCopy(s []int, n int) {
+	sCopy := make([]int, n)
+	copy(sCopy, s[:n])          // copy before n to another slice
+	copy(s, s[n:])              // copy after n to the begin of slice
+	copy(s[len(s)-n:], sCopy, ) // copy before n from another slice to the end of current
+}
+
+func rotate3(s []int, n int) {
+	// use pop and push
+}
--- exercises/data_structures/slices_test.go
+++ exercises/data_structures/slices_test.go
@@ -2,6 +2,7 @@ package data_structures
 
 import (
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -17,4 +18,23 @@ func Test_reverseArray(t *testing.T) {
 	input := []int{1, 0, 3, 4}
 	reverseArray(&input)
 	assert.Equal(t, input, expected)
+}
+
+func Test_rotate(t *testing.T) {
+	input := []int{1, 2, 3, 4}
+	rotate(input, 2)
+	assert.Equal(t, input, []int{3, 4, 1, 2})
+}
+func Test_rotateByCopy(t *testing.T) {
+	input := []int{1, 2, 3, 4}
+	rotateByCopy(input, 2)
+	assert.True(t, reflect.DeepEqual([]int{3, 4, 1, 2}, input))
+
+	input3 := []int{1, 2, 3, 4, 5, 6, 7}
+	rotateByCopy(input3, 2)
+	assert.True(t, reflect.DeepEqual([]int{3, 4, 5, 6, 7, 1, 2}, input3))
+
+	input2 := []int{1, 2, 3, 4, 5, 6, 7}
+	rotateByCopy(input2, 5)
+	assert.True(t, reflect.DeepEqual([]int{6, 7, 1, 2, 3, 4, 5}, input2))
 }
