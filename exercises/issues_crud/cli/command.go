2b76649381b49a88a18fbec5b5388a29d5c50821
Sergiy Sych <sychsergiy@gmail.com>
2019-08-27 11:05:39 +0300

    trying to implement abstract method

--- exercises/issues_crud/cli/command.go
+++ exercises/issues_crud/cli/command.go
@@ -1,5 +1,27 @@
 package cli
 
+import (
+	"log"
+	"os"
+)
+
 type Command interface {
 	Execute()
+}
+
+type BaseCommand struct {
+}
+
+func (command *BaseCommand) RetrieveArgs(argsLen int) []string {
+	if len(os.Args) < argsLen {
+		log.Fatal("Not enough arguments")
+	}
+	args := make([]string, 0, argsLen)
+
+	for index, value := range os.Args {
+		if index > 1 {
+			args = append(args, value)
+		}
+	}
+	return args
 }
--- exercises/issues_crud/commands/lock.go
+++ exercises/issues_crud/commands/lock.go
@@ -2,16 +2,39 @@ package commands
 
 import (
 	"log"
+	"os"
+	"overview/exercises/issues_crud/cli"
 	"overview/exercises/issues_crud/github"
+	"strconv"
 )
 
 type LockIssue struct {
+	BaseCommand  cli.BaseCommand
 	GithubClient github.Client
 }
 
 func (handler *LockIssue) Execute() {
-	err := handler.GithubClient.LockIssue("LearnGo", 1, "any reason")
+	args := handler.BaseCommand.RetrieveArgs(3)
+	issueNumber, err := strconv.Atoi(args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	err2 := handler.GithubClient.LockIssue(args[0], issueNumber, args[2])
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+}
+
+func (handler *LockIssue) parseArgs() (string, int, string) {
+	if len(os.Args) < 5 {
+		log.Fatal("Not enough arguments")
+	}
+	repo := os.Args[2]
+	issueNumber, err := strconv.Atoi(os.Args[3])
+	reason := os.Args[4]
+	if err != nil {
+		log.Fatal(err)
+	}
+	return repo, issueNumber, reason
 }
