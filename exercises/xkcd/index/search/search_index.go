95e593c2c013ad24420b4e17b52c450cb4c9475c
Sergiy Sych <sychsergiy@gmail.com>
2019-09-03 16:12:39 +0300

    search index

---
+++ exercises/xkcd/index/search/search_index.go
@@ -0,0 +1,86 @@
+package search
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+	"xkcd/comic"
+)
+
+func contains(slice []string, item string) bool {
+	for _, excludeWord := range slice {
+		if strings.Compare(item, excludeWord) == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func cleanPunctuation(text string) string {
+	reg, err := regexp.Compile("[^A-Za-z0-9]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return reg.ReplaceAllString(text, " ")
+}
+
+func getKeyWords(text string, wordsToExclude []string) []string {
+	text = strings.ToLower(text)
+	words := strings.Split(cleanPunctuation(text), " ")
+	filteredWords := make([]string, 0, len(words))
+	for _, word := range words {
+		if !contains(wordsToExclude, word) {
+			filteredWords = append(filteredWords, word)
+		}
+	}
+	return filteredWords
+}
+
+type ComicIndexItem struct {
+	TitleKeyWords      []string
+	TranscriptKeyWords []string
+	Num                int
+}
+
+func CreateComicIndexItem(comic comic.Comic) *ComicIndexItem {
+	excluded := []string{"a", "the", "an"}
+	titleKeyWords := getKeyWords(comic.Title, excluded)
+	transcriptKeyWords := getKeyWords(comic.Transcript, excluded)
+	sort.Strings(titleKeyWords)
+	sort.Strings(transcriptKeyWords)
+	return &ComicIndexItem{TitleKeyWords: titleKeyWords, TranscriptKeyWords: titleKeyWords, Num: comic.Num}
+}
+
+func ReadComicIndex() []ComicIndexItem {
+	file, err := os.OpenFile("search_index.json", os.O_RDWR|os.O_CREATE, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content, err := ioutil.ReadAll(file)
+	indexItems := make([]ComicIndexItem, 0, 0)
+	err = json.Unmarshal(content, &indexItems)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return indexItems
+}
+
+func WriteComicIndex(comicIndexItems []ComicIndexItem) {
+	file, err := os.OpenFile("search_index.json", os.O_WRONLY|os.O_CREATE, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jsonData, err := json.Marshal(comicIndexItems)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = file.Write(jsonData)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
