c892dfac35dede408608f959300a2de05551a16b
Sergiy Sych <sychsergiy@gmail.com>
2019-09-11 11:44:25 +0300

    improve search

--- exercises/xkcd/search/searcher.go
+++ exercises/xkcd/search/searcher.go
@@ -4,53 +4,62 @@ import (
 	"sort"
 	"strings"
 	"xkcd/search/comic_index"
+	"xkcd/search/util"
 )
 
 type searchItem struct {
 	Num          int
 	MatchPercent int
-	Field        string
 }
 
-func Search(index comic_index.ComicIndex, query string) []int {
-	// todo: create set of words from query(only one word now)
-	var searchItems []searchItem
-	items := index.ReadAll()
+func calcMatchPercent(words []string, queryWords []string) int {
+	var matchPercentPerWord, matchPercentSum int
+	matchPercentPerWord = 100 / len(queryWords)
 
-
-	for _, item := range items {
-
-		for _, titleKeyWord := range item.TitleKeyWords {
-			if strings.Contains(titleKeyWord, query) {
-				searchItems = append(searchItems, searchItem{item.Num, 100, "title"})
-				break
-			}
-		}
-
-		for _, transcriptKeyWord := range item.TranscriptKeyWords {
-			if strings.Contains(transcriptKeyWord, query) {
-				searchItems = append(searchItems, searchItem{item.Num, 100, "description"})
-				break
+	matchPercentSum = 0
+	for _, word := range words {
+		for _, queryWord := range queryWords {
+			if strings.Contains(word, queryWord) {
+				matchPercentSum += matchPercentPerWord
 			}
 		}
 	}
+	return matchPercentSum
+}
 
-	sort.Slice(searchItems, func(i, j int) bool {
-		if searchItems[i].Field == "description" && searchItems[j].Field == "title" {
-			return true
-		} else if searchItems[i].Field == "title" && searchItems[j].Field == "description" {
-			return false
-		} else {
-			return false
+func Search(index comic_index.ComicIndex, query string) []int {
+	var matchPercent int
+	var titleMatchedItems, descriptionMatchedItems []searchItem
+	items := index.ReadAll()
+
+	queryWords := util.GetKeyWords(query, []string{})
+
+	for _, item := range items {
+		matchPercent = calcMatchPercent(item.TitleKeyWords, queryWords)
+		if matchPercent > 0 {
+			titleMatchedItems = append(titleMatchedItems, searchItem{item.Num, matchPercent})
+			break
 		}
+
+		matchPercent = calcMatchPercent(item.TranscriptKeyWords, queryWords)
+		if matchPercent > 0 {
+			descriptionMatchedItems = append(descriptionMatchedItems, searchItem{item.Num, matchPercent})
+		}
+	}
+
+	sort.Slice(titleMatchedItems, func(i, j int) bool {
+		return titleMatchedItems[i].MatchPercent < titleMatchedItems[j].MatchPercent
+	})
+	sort.Slice(descriptionMatchedItems, func(i, j int) bool {
+		return descriptionMatchedItems[i].MatchPercent < descriptionMatchedItems[j].MatchPercent
 	})
 
-	sort.Slice(searchItems, func(i, j int) bool {
-		return searchItems[i].MatchPercent < searchItems[j].MatchPercent
-	})
+	nums := make([]int, 0, len(titleMatchedItems)+len(descriptionMatchedItems))
+	for _, searchItem := range titleMatchedItems {
+		nums = append(nums, searchItem.Num)
+	}
 
-	nums := make([]int, 0, len(searchItems))
-	for _, searchItem := range searchItems {
+	for _, searchItem := range descriptionMatchedItems {
 		nums = append(nums, searchItem.Num)
 	}
 	return nums
