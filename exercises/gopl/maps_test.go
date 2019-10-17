9f736a09145ed262e15d21daa6fcf46d7730e9cb
Sergiy Sych <sychsergiy@gmail.com>
2019-10-17 09:07:02 +0300

    rename dir

--- exercises/gopl/maps_test.go
+++ exercises/gopl/maps_test.go
@@ -1,4 +1,4 @@
-package data_structures
+package gopl
 
 import (
 	"github.com/stretchr/testify/assert"
--- exercises/gopl/slices.go
+++ exercises/gopl/slices.go
@@ -1,4 +1,4 @@
-package data_structures
+package gopl
 
 func reverse(s []int) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
