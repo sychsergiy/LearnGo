499ef6379e93bd247c20f19f8736d4af4fdf0d58
Sergiy Sych <sychsergiy@gmail.com>
2019-11-03 08:59:37 +0200

    concurrent safe bank withdraw method

--- exercises/gopl/concurrency/bank/bank.go
+++ exercises/gopl/concurrency/bank/bank.go
@@ -1,11 +1,23 @@
 // Package bank provides a concurrency-safe bank with one account.
-package bank
+package main
 
 var deposits = make(chan int) // send amount to deposit
 var balances = make(chan int) // receive balance
+var withdraws = make(chan withdrawData)
 
 func Deposit(amount int) { deposits <- amount }
 func Balance() int       { return <-balances }
+
+type withdrawData struct {
+	amount   int
+	resultCh chan bool
+}
+
+func Withdraw(amount int) bool {
+	resultCh := make(chan bool)
+	withdraws <- withdrawData{amount, resultCh}
+	return <-resultCh
+}
 
 func teller() {
 	var balance int // balance is confined to teller goroutine
@@ -14,6 +26,13 @@ func teller() {
 		case amount := <-deposits:
 			balance += amount
 		case balances <- balance:
+		case withdrawData := <-withdraws:
+			if balance > withdrawData.amount {
+				balance -= withdrawData.amount
+				withdrawData.resultCh <- true
+			} else {
+				withdrawData.resultCh <- false
+			}
 		}
 	}
 }
